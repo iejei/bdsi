38ae8063aaecf85e36138c62e29827bd04fc3719
iejei <917121917@qq.com>
2021-04-19 15:38:26 +0800

    添加项目

---
+++ src/main/java/com/fslu/bigdata/application/Application.java
@@ -0,0 +1,16 @@
+//package com.fslu.bigdata.application;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@ComponentScan("com.fslu")
+////@ServletComponentScan
+//@SpringBootApplication/*(exclude = PageHelperAutoConfiguration.class)*/
+//public class Application {
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//}
